baf880de86f3f867dad7b559292533284993a94b
echocslsk <CardioLeo@gmail.com>
2024-03-10 07:14:01 -0600

    makes draw_board into something that now prints the actual values in the board_spots array; so that once I get input resolved, it can actually show updated boards; this commit is based on several hours of work trying to figure out all sorts of nuances, like how to get items of different kinds to print on the same line! Also, changing spacing a decent amount. This was a lot of work, but I think it brings the draw_board function up to a good level of functionality. That function may not need any more updates.

--- tictactoe.lisp
+++ tictactoe.lisp
@@ -18,9 +18,8 @@
 (defvar *board_spots*)
 ; play_again_input, spot_to_fill
 
-(defun test-board-vals ()
+(defun set-board-values ()
   (setf *board_spots* (make-array '(9)))
-  (terpri)
   (setf (aref *board_spots* 0) '1)
   (setf (aref *board_spots* 1) '2)
   (setf (aref *board_spots* 2) '3)
@@ -29,10 +28,8 @@
   (setf (aref *board_spots* 5) '6)
   (setf (aref *board_spots* 6) '7)
   (setf (aref *board_spots* 7) '8)
-  (setf (aref *board_spots* 8) '9)
-  (write *board_spots*)
-  (terpri))
-
+  (setf (aref *board_spots* 8) '9))
+  ;(write *board_spots*)
 ; test function list main functions I need to create and test
 
 ;(defun my-t3-list ()
@@ -54,8 +51,7 @@
   (terpri)
   (if (= *round_counter* 9) (and (and (format t '"Last Round!") (and (terpri) (terpri)))))
   (terpri)
-  (format t '"Current Round is: ")
-  (print *round_counter*)
+  (format t '"Current Round is: ")(print *round_counter*)
   (terpri)
   (format t '"Current Turn goes to: ")
   (print *mark*)
@@ -65,31 +61,50 @@
   (if (= 1 (mod *round_counter* 2)) (setf *mark* 'X))
   (if (= 0 (mod *round_counter* 2)) (setf *mark* 'O)))
 
+(defun printem (&rest args)
+      (format t '"~{~a~^ ~}" args))
+
 (defun draw-board ()
-  ;(my-t3-list)
-  ;(my-functions)
-  ;'(announce-three)
-  (format t '"
+  (terpri)
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (printem '"  " (aref *board_spots* 0) '"  !  " (aref *board_spots* 1) '"  !  " (aref *board_spots* 2))
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (format t '"_______!_______!_______")
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (printem '"  " (aref *board_spots* 3) '"  !  " (aref *board_spots* 4) '"  !  " (aref *board_spots* 5))
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (format t '"_______!_______!_______")
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (printem '"  " (aref *board_spots* 6) '"  !  " (aref *board_spots* 7) '"  !  " (aref *board_spots* 8))
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (format t '"       !       !       ")
+  (terpri)
+  (terpri))
 
-
-     |     |     
-  1  |  2  |  3  
-_____|_____|_____
-     |     |     
-  4  |  5  |  6  
-_____|_____|_____
-     |     |     
-  7  |  8  |  9  
-     |     |     
-
-
-")(terpri)
-  ;(setf *game_over* 'true *found_a_match* 'true)
+;(setf *game_over* 'true *found_a_match* 'true)
   ;(print '"The variable game_over is set to: ")
   ;(print *game_over*)
   ;(print '"and the varialble found_a_match is set to: ")
   ;(print *found_a_match*)
-  (terpri))
+  ;(terpri)
 
 (defun player-move ()
   (format t '"Please select a spot to place your mark:")
@@ -134,11 +149,14 @@ _____|_____|_____
   (display-info))
 
 (defun start-game ()
+  (set-board-values)
+  (draw-board)
   (loop while (< *round_counter* 9)
         do (setf *round_counter* (+ *round_counter* 1))
            (terpri)(terpri)
 	   (early-helper-functions)
 	   (player-move)
+	   ;(test-input-draw-board)
 	   (draw-board)
 	   (late-helper-functions)))
 
@@ -146,26 +164,33 @@ _____|_____|_____
   (format t '"Resetting values so game can restart")
   (setf *game_in_play* t)
   (setf *round_counter* 0)
-  (setf (aref *board_spots* 1) '2)
-  (setf (aref *board_spots* 2) '3)
-  (setf (aref *board_spots* 3) '4)
-  (setf (aref *board_spots* 4) '5)
-  (setf (aref *board_spots* 5) '6)
-  (setf (aref *board_spots* 6) '7)
-  (setf (aref *board_spots* 7) '8)
-  (setf (aref *board_spots* 8) '9))
+  (set-board-values))
+
+(defun take-endgame-input ()
+  (let ((p nil))
+    (setf p (read))
+    (print (append '(You said ) (list p)))
+    (terpri)
+    (terpri)
+    (format t '"Okay, restarting now!")
+    (terpri)
+    (terpri)
+    (terpri)))
+; if yes, call reset-pregame-variables, then start-game
+
+(defun speak-question ()
+  (format t '"Would you like to play again?"))
 
 (defun ask-to-play ()
-  (format t '"Would you like to play again?")
-  (terpri)
-  (terpri))
-; if yes, call reset-pregame-variables, then start-game
+  (speak-question)
+  (take-endgame-input)
+  (reset-pregame-variables)
+  (start-game))
 
 (defun main-play-loop ()
   (setf *game_in_play* t)
   (loop while *game_in_play*
         do (terpri)(terpri)
-	(draw-board)
 	(start-game)
 	(ask-to-play)
 	))
